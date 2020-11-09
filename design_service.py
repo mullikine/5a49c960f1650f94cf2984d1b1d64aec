cf1ad12e5e5503d1789c27b3ac4fdb9a7d28452d
Shane Mulligan <mullikine@gmail.com>
2020-11-10 12:05:32 +1300

    10.11.20

---
+++ design_service.py
@@ -0,0 +1,25 @@
+# System Architecture: User -> Auth Server -> ctx -> service.method(ctx, params…)
+# Design contents are immutable after created
+# DesignService.methods(ctx,.......)
+
+# service = DesignService()
+# foo_id = service.create_design(ctx, 'foo')
+# service.get_design(ctx, foo_id) -> 'foo'
+
+
+from typing import List, Iterable
+
+# Identifies the caller of a service method.
+class AuthContext:
+    def __init__(self, user_id: str):
+        self.user_id = user_id  # E.g. ‘userAbc’
+
+# Any calls made to this service are authenticated.
+class DesignServiceInterface:
+    def create_design(self, ctx: AuthContext, design_content: str) -> str:
+        """Creates a design and returns the design id"""
+        raise NotImplementedError
+
+    def get_design(self, ctx: AuthContext, design_id: str) -> str:
+        """Returns the design content, if the user has access to the design."""
+        raise NotImplementedError
